1088ec9c1c7c7d3d8623797894f66a654d458f89
Alec <alecsantos96@gmail.com>
2021-06-07 18:10:41 -0300

    Refazendo tudo pra ficar mais fácil

--- Main.gd
+++ Main.gd
@@ -1,42 +1,76 @@
-extends Node2D
+extends Control
 
-onready var botao_pedra = $BotaoPedra
-onready var botao_papel = $BotaoPapel
-onready var botao_tesoura = $BotaoTesoura
-#onready var label_1 = $Label1
-onready var label_2 = $Label2
+var escolha_jogador_1 = 0 # 0 é pedra, 1 é papel, 2 é tesoura
+var escolha_jogador_2 = 0
 
-func _ready():
-	botao_pedra.connect("button_down", self, "jogar",[0])
-	botao_papel.connect("button_down", self, "jogar",[1])
-	botao_tesoura.connect("button_down", self, "jogar",[2])
+func _ready(): #Chamado no inicio do programa
+	# Conectando botões a funções
+	$BotaoPedra1.connect("button_down", self, "botao_pedra_1_down")
+	$BotaoPapel1.connect("button_down", self, "botao_papel_1_down")
+	$BotaoTesoura1.connect("button_down", self, "botao_tesoura_1_down")
+	$BotaoPedra2.connect("button_down", self, "botao_pedra_2_down")
+	$BotaoPapel2.connect("button_down", self, "botao_papel_2_down")
+	$BotaoTesoura2.connect("button_down", self, "botao_tesoura_2_down")
+
+func botao_pedra_1_down():
+	escolha_jogador_1 = 0
+
+func botao_papel_1_down():
+	escolha_jogador_1 = 1
+
+func botao_tesoura_1_down():
+	escolha_jogador_1 = 2
+
+func botao_pedra_2_down():
+	escolha_jogador_2 = 0
+
+func botao_papel_2_down():
+	escolha_jogador_2 = 1
+
+func botao_tesoura_2_down():
+	escolha_jogador_2 = 2
 
 
-func jogar(escolha_jogador):
-	randomize()
-	var escolha_cpu = randi()%3 #valor aleatório de 0 a 2
-	match escolha_jogador:
-		0: #jogador escolheu pedra
-			match escolha_cpu:
-				0: #cpu escolheu pedra
-					label_2.text = "Você jogou pedra contra pedra e empatou."
+func _process(delta): #Chamado a cada frame
+	#Atualizar o texto do jogador 1
+	if escolha_jogador_1 == 0:
+		$LabelJogador1.text = "Jogador 1: Pedra"
+	elif escolha_jogador_1 == 1:
+		$LabelJogador1.text = "Jogador 1: Papel"
+	else:
+		$LabelJogador1.text = "Jogador 1: Tesoura"
+	#Atualizar o texto do jogador 2
+	match escolha_jogador_2:
+		0:
+			$LabelJogador2.text = "Jogador 2: Pedra"
+		1:
+			$LabelJogador2.text = "Jogador 2: Papel"
+		2:
+			$LabelJogador2.text = "Jogador 2: Tesoura"
+	#Atualizar texto do vencedor
+	match escolha_jogador_1:
+		0: #jogador 1 escolheu pedra
+			match escolha_jogador_2:
+				0: #jogador 2 escolheu pedra
+					$LabelVencedor.text = "Empate."
 				1: #cpu escolheu papel
-					label_2.text = "Você jogou pedra contra papel e perdeu."
+					$LabelVencedor.text = "Vencedor: Jogador 1."
 				2: #cpu escolheu tesoura
-					label_2.text = "Você jogou pedra contra tesoura e ganhou."
-		1: #jogador escolheu papel
-			match escolha_cpu:
+					$LabelVencedor.text = "Vencedor: Jogador 2."
+		1: #jogador 1 escolheu papel
+			match escolha_jogador_2:
 				0: #cpu escolheu pedra
-					label_2.text = "Você jogou papel contra pedra e ganhou."
+					$LabelVencedor.text = "Vencedor: Jogador 1."
 				1: #cpu escolheu papel
-					label_2.text = "Você jogou papel contra papel e empatou."
+					$LabelVencedor.text = "Empate."
 				2: #cpu escolheu tesoura
-					label_2.text = "Você jogou papel contra tesoura e perdeu."
-		2: #jogador escolheu tesoura
-			match escolha_cpu:
+					$LabelVencedor.text = "Vencedor: Jogador 2."
+		2: #jogador 1 escolheu tesoura
+			match escolha_jogador_2:
 				0: #cpu escolheu pedra
-					label_2.text = "Você jogou tesoura contra pedra perdeu."
+					$LabelVencedor.text = "Vencedor: Jogador 2."
 				1: #cpu escolheu papel
-					label_2.text = "Você jogou tesoura contra papel e venceu."
+					$LabelVencedor.text = "Vencedor: Jogador 1."
 				2: #cpu escolheu tesoura
-					label_2.text = "Você jogou tesoura contra tesoura e empatou."
+					$LabelVencedor.text = "Empate."
+
